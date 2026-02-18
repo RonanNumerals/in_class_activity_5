b52052a7fe49b3d9ff86a65e6b5952fd5cbc079a
Ronan Pelot <ronanpelot@gmail.com>
2026-02-18 14:29:33 -0500

    Added hunger timer and name text box

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
   runApp(MaterialApp(
@@ -6,15 +7,32 @@ void main() {
   ));
 }
 
+
 class DigitalPetApp extends StatefulWidget {
   @override
   _DigitalPetAppState createState() => _DigitalPetAppState();
 }
 
 class _DigitalPetAppState extends State<DigitalPetApp> {
-  String petName = "Your Pet";
+  String petName = "";
   int happinessLevel = 50;
   int hungerLevel = 50;
+  Timer? _timer;
+  final _textController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(Duration(seconds: 30), (timer) {
+      _updateHunger();
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
 
   void _playWithPet() {
     setState(() {
@@ -48,6 +66,22 @@ class _DigitalPetAppState extends State<DigitalPetApp> {
     });
   }
 
+  Color _moodColor(int happinessLevel) {
+    if (happinessLevel > 70) {
+      return Colors.green;
+    } else if (happinessLevel >= 30) {
+      return Colors.yellow;
+    } else {
+      return Colors.red;
+    }
+  }
+
+  void _petName() {
+    setState(() {
+      petName = _textController.text;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,8 +92,46 @@ class _DigitalPetAppState extends State<DigitalPetApp> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 200.0,
+                  height: 50.0,
+                  child:
+                    TextField(
+                      controller: _textController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter your pet\'s name',
+                      ),
+                    )
+                  ),
+                SizedBox(width: 8.0),
+                ElevatedButton(
+                  onPressed: () {
+                    _petName();
+                  },
+                  child: const Text('Enter'),
+                ),
+              ]
+            ),   
             Text('Name: $petName', style: TextStyle(fontSize: 20.0)),
             SizedBox(height: 16.0),
+            ColorFiltered(
+              colorFilter: ColorFilter.mode(
+                _moodColor(happinessLevel),
+                BlendMode.modulate,
+              ),
+              child: Image.asset('assets/pet_image.png', width: 50, height: 50),
+            ),
+            SizedBox(height: 16.0),
+            if (happinessLevel > 70)
+              Text('Happy! 😄', style: TextStyle(fontSize: 18.0, color: Colors.green)),
+            if (happinessLevel >= 30 && happinessLevel <= 70)
+              Text('Neutral 😐', style: TextStyle(fontSize: 18.0, color: Colors.yellow)),
+            if (happinessLevel < 30)
+              Text('Sad 😢', style: TextStyle(fontSize: 18.0, color: Colors.red)),
             Text('Happiness Level: $happinessLevel', style: TextStyle(fontSize: 20.0)),
             SizedBox(height: 16.0),
             Text('Hunger Level: $hungerLevel', style: TextStyle(fontSize: 20.0)),
